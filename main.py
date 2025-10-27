506080a71dc436ec92271a8d1132314c4aa1d263
Faris4166 <farisarthan@gmail.com>
2025-10-27 20:28:12 +0700

    dele

--- main.py
+++
@@ -1,7 +0,0 @@
-from ui_main import SashaApp
-import customtkinter as ctk
-
-if __name__ == "__main__":
-    root = ctk.CTk()
-    app = SashaApp(root)
-    root.mainloop()
--- utils.py
+++
@@ -1,9 +0,0 @@
-import base64, io
-
-def image_to_bytes(pil_image):
-    """
-    แปลง PIL Image object ให้เป็น bytes buffer สำหรับส่งไปยัง Gemini API โดยตรง
-    """
-    buf = io.BytesIO()
-    pil_image.save(buf, format="JPEG")
-    return buf.getvalue()
